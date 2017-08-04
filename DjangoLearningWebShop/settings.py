0db9f357dd2ba5dbd2facb5718e81a43da564ae6
zok1995 <zokzok95@gmail.com>
2017-08-04 13:55:05 +0300

    lesson 1.1 finished

--- DjangoLearningWebShop/settings.py
+++ DjangoLearningWebShop/settings.py
@@ -27,10 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-#Allow static dirs
-STATICFILES_DIRS =(os.path.join(BASE_DIR,'static',),)
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -122,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, "static",),
+)
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+import time
 # Create your views here.
 
 def landing(request):
+    name = "Oleksandr"
+    currentDay = time.strftime("%H:%M:%S")
     return render(request, 'landing/landing.html', locals())
