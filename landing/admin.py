8e2daddb1a67da1c06aa5bb993deb6bf405cc135
zok1995 <zokzok95@gmail.com>
2017-08-07 11:51:39 +0300

    cusctomiying admin page, model Subscriber renamed

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Subscribers)
+class SubscriberAdmin(admin.ModelAdmin):
+    # list_display = ["name", "email"] #hand made
+    list_display = [field.name for field in Subscriber._meta.fields] #use itertor for every field name from DB
+    # exclude = ["email"] #hide field
+    list_filter = ['name', 'email']  #filter record in admin
+    search_fields = ["name"] #find by in admin
+
+    class Meta:
+        model = Subscriber
+
+admin.site.register(Subscriber, SubscriberAdmin)
--- landing/models.py
+++ landing/models.py
@@ -2,6 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Subscribers(models.Model):
+class Subscriber(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=128)
+
+    def __str__(self):  #customize django admin
+        return "Meail: %s, User: %s" % (self.email, self.name)
+
+    class Meta: #Changing name of the model in the DB
+        verbose_name = 'Subscriber'
+        verbose_name_plural = 'Subscribers'
